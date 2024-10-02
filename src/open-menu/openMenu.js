a4a9ca6e62aa945f006fef4fe599064435b64ca5
Artem Bykov <bykovaa94@mail.ru>
2024-10-02 14:22:09 +0300

    fix name button

--- src/open-menu/openMenu.js
+++ src/open-menu/openMenu.js
@@ -2,7 +2,7 @@ export const openMenu = () => {
   const button = document.querySelector('#button');
   const menu = document.querySelector('.castom-swiper__wrapper');
   const buttonText = document.querySelector('.button__text');
-  const buttonIcon = document.querySelector('.button-icon');
+  const buttonIcon = document.querySelector('.button__icon');
 
 
   button.addEventListener('click', () => {
@@ -14,13 +14,13 @@ export const openMenu = () => {
       menu.classList.add('menu_open');
       button.classList.remove('close');
       button.classList.add('open');
-      buttonIcon.classList.remove('button-icon_rotate-close');
-      buttonIcon.classList.add('button-icon_rotate-open');
+      buttonIcon.classList.remove('button__icon_rotate-close');
+      buttonIcon.classList.add('button__icon_rotate-open');
       buttonText.textContent = 'Скрыть';
     }
     if (!isClose) {
-      buttonIcon.classList.remove('button-icon_rotate-open');
-      buttonIcon.classList.add('button-icon_rotate-close');
+      buttonIcon.classList.remove('button__icon_rotate-open');
+      buttonIcon.classList.add('button__icon_rotate-close');
       menu.classList.remove('menu_open');
       menu.classList.add('menu_close');
       button.classList.remove('open');
